4743c231d7f54b2e054642c9fd47e48f1234d851
amrutsabale <samrut355@gmail.com>
2023-01-22 16:29:11 +0530

    Implemented dates range picker and search UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,78 @@ import {
   GlobeAltIcon,
   MenuIcon,
   UserCircleIcon,
-  UserIcon,
+  UsersIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useRouter } from "next/router";
 
-function Header() {
+type Selection = {
+  startDate: Date;
+  endDate: Date;
+  color: string;
+  key: string;
+  autoFocus: boolean;
+  disabled: boolean;
+  showDateDisplay: boolean;
+};
+
+type HeaderProps = {
+  placeholder?: string;
+};
+
+function Header({ placeholder = "" }: HeaderProps) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: "selection",
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleDateRangeSelect = ({ selection }: { selection: Selection }) => {
+    setStartDate(selection.startDate);
+    setEndDate(selection.endDate);
+  };
+
+  const handleNoOfGuestsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNoOfGuests(Number(e.target.value));
+  };
+
+  const handleResetInput = () => {
+    setSearchInput("");
+  };
+
+  const goSearch = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
   return (
     <header
       className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5
     md:px-10"
     >
-      <div className="relative flex items-center h-10 cursor-pointer">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           alt="logo"
@@ -24,7 +86,9 @@ function Header() {
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           type="text"
-          placeholder="Start your Search"
+          placeholder={placeholder || "Start your Search"}
+          value={searchInput}
+          onChange={handleSearch}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
@@ -37,6 +101,40 @@ function Header() {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-1">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleDateRangeSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl pl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              value={noOfGuests}
+              onChange={handleNoOfGuestsChange}
+              min={1}
+            />
+          </div>
+          <div className="flex">
+            <button
+              onClick={handleResetInput}
+              className="flex-grow text-gray-500"
+            >
+              Cancel
+            </button>
+            <button onClick={goSearch} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
---
+++ pages/search.tsx
@@ -0,0 +1,42 @@
+import { format } from "date-fns";
+import { useRouter } from "next/router";
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+  const formattedStartDate =
+    typeof startDate === "string"
+      ? format(new Date(startDate), "dd MMM yy")
+      : "";
+  const formattedEndDate =
+    typeof endDate === "string" ? format(new Date(endDate), "dd MMM yy") : "";
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancelation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
